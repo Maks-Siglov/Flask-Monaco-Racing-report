d7e6d98d5889005ef6c5b3f3b0769aeec9e97eff
Maks <gmb333x2@gmail.com>
2023-10-18 11:45:51 +0300

    RequestParser and version added to api

--- app/api/report.py
+++ app/api/report.py
@@ -4,33 +4,39 @@ from app.api.utils import json_data_for_report, json_data_for_driver
 
 from werkzeug.exceptions import HTTPException
 from flask import request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 api = Api(app)
 
 PREPARED_DATA = prepare()
 
+parser = reqparse.RequestParser()
+parser.add_argument('format', type=str, location='args', default='json')
+parser.add_argument('order', type=str, location='args', default='asc')
+
 
 class Report(Resource):
     def get(self):
-        order = request.args.get('order')
-        if order == 'desc':
+        args = parser.parse_args()
+        if args['order'] == 'desc':
             PREPARED_DATA.reverse()
 
-        json_data = json_data_for_report(PREPARED_DATA)
-
-        return json_data
+        if args['format'] == 'json':
+            return json_data_for_report(PREPARED_DATA)
+        else:
+            pass
 
 
 class Drivers(Resource):
     def get(self):
-        order = request.args.get('order')
-        if order == 'desc':
+        args = parser.parse_args()
+        if args['order'] == 'desc':
             PREPARED_DATA.reverse()
 
-        json_data = json_data_for_report(PREPARED_DATA)
-
-        return json_data
+        if args['format'] == 'json':
+            return json_data_for_report(PREPARED_DATA)
+        else:
+            pass
 
 
 class UniqueDriver(Resource):
@@ -45,9 +51,9 @@ class UniqueDriver(Resource):
         raise HTTPException(response='404')
 
 
-api.add_resource(Report, '/api/report/')
-api.add_resource(Drivers, '/api/report/drivers/')
-api.add_resource(UniqueDriver, '/api/report/drivers/<string:driver_id>/')
+api.add_resource(Report, '/api/v1/report')
+api.add_resource(Drivers, '/api/v1/report/drivers/')
+api.add_resource(UniqueDriver, '/api/v1/report/drivers/<string:driver_id>/')
 
 
 if __name__ == '__main__':
