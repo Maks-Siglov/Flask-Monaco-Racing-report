84ca14db7215e01353af4539c58c1d20ec787b8a
Maks <gmb333x2@gmail.com>
2023-10-18 16:59:57 +0300

    xml format added to api

--- app/api/report.py
+++ app/api/report.py
@@ -1,9 +1,12 @@
 from app.app import app
 from app.bl.report.prepare import prepare
-from app.api.utils import json_data_for_report, json_data_for_driver
-
-from werkzeug.exceptions import HTTPException
-from flask import request
+from app.api.utils import (
+    json_data_for_report,
+    json_data_for_driver,
+    xml_response_for_report,
+    xml_response_for_driver
+)
+from flask import make_response, render_template, Response
 from flask_restful import Api, Resource, reqparse
 
 api = Api(app)
@@ -16,39 +19,41 @@ parser.add_argument('order', type=str, location='args', default='asc')
 
 
 class Report(Resource):
-    def get(self):
+    def get(self) -> dict | Response:
         args = parser.parse_args()
         if args['order'] == 'desc':
             PREPARED_DATA.reverse()
 
-        if args['format'] == 'json':
-            return json_data_for_report(PREPARED_DATA)
-        else:
-            pass
+        if args['format'] == 'xml':
+            return xml_response_for_report(PREPARED_DATA)
+
+        return json_data_for_report(PREPARED_DATA)
 
 
 class Drivers(Resource):
-    def get(self):
+    def get(self) -> dict | Response:
         args = parser.parse_args()
         if args['order'] == 'desc':
             PREPARED_DATA.reverse()
 
-        if args['format'] == 'json':
-            return json_data_for_report(PREPARED_DATA)
-        else:
-            pass
+        if args['format'] == 'xml':
+            return xml_response_for_report(PREPARED_DATA)
+
+        return json_data_for_report(PREPARED_DATA)
 
 
 class UniqueDriver(Resource):
-    def get(self, driver_id: str):
+    def get(self, driver_id: str) -> dict | Response:
+        args = parser.parse_args()
         for driver in PREPARED_DATA:
             if driver.abr == driver_id:
 
-                json_data = json_data_for_driver(driver)
+                if args['format'] == 'xml':
+                    return xml_response_for_driver(driver)
 
-                return json_data
+                return json_data_for_driver(driver)
 
-        raise HTTPException(response='404')
+        return make_response(render_template('404.html'), 404)
 
 
 api.add_resource(Report, '/api/v1/report')
--- app/api/utils.py
+++ app/api/utils.py
@@ -1,26 +1,68 @@
 from app.bl.report.models import Driver
+from xml.etree import ElementTree
+from flask import make_response, Response
 
 
 def json_data_for_report(prepared_data: list[Driver]) -> dict:
-    json_data = {
-        driver.abr: {
+    return {
+        driver.abr: _prepare_driver_for_json(driver) for driver in prepared_data
+            }
+
+
+def json_data_for_driver(driver: Driver) -> dict:
+    return {driver.abr: _prepare_driver_for_json(driver)}
+
+
+def _prepare_driver_for_json(driver: Driver) -> dict:
+    json_driver = {
             'position': driver.position,
             'name': driver.name,
             'team': driver.team,
             'lap_time': [driver.lap_time.minutes, driver.lap_time.seconds]
-        }
-        for driver in prepared_data}
-
-    return json_data
-
-
-def json_data_for_driver(driver: Driver) -> dict:
-    lap_time = [driver.lap_time.minutes, driver.lap_time.seconds]
-    json_data = {
-        'position': driver.position,
-        'abr': driver.abr,
-        'name': driver.name,
-        'team': driver.team,
-        'lap_time': lap_time
     }
-    return json_data
+    return json_driver
+
+
+def xml_response_for_report(prepared_data: list[Driver]) -> Response:
+    root = ElementTree.Element('drivers')
+
+    for driver in prepared_data:
+        driver_element = ElementTree.SubElement(root, 'driver')
+
+        _prepare_driver_for_xml(driver_element, driver)
+
+    response = make_response(
+        ElementTree.tostring(root, encoding='utf-8').decode())
+    response.headers['Content-Type'] = 'application/xml'
+
+    return response
+
+
+def xml_response_for_driver(driver: Driver) -> Response:
+    root = ElementTree.Element('driver')
+
+    _prepare_driver_for_xml(root, driver)
+
+    response = make_response(
+        ElementTree.tostring(root, encoding='utf-8').decode())
+    response.headers['Content-Type'] = 'application/xml'
+
+    return response
+
+
+def _prepare_driver_for_xml(root: ElementTree.Element, driver: Driver) -> None:
+
+    position = ElementTree.SubElement(root, 'position')
+    abr = ElementTree.SubElement(root, 'abr')
+    name = ElementTree.SubElement(root, 'name')
+    team = ElementTree.SubElement(root, 'team')
+    lap_time_element = ElementTree.SubElement(root, 'lap_time')
+    minutes = ElementTree.SubElement(lap_time_element, 'minutes')
+    seconds = ElementTree.SubElement(lap_time_element, 'seconds')
+
+    position.text = str(driver.position)
+    abr.text = driver.abr
+    name.text = driver.name
+    team.text = driver.team
+    minutes.text = str(driver.lap_time.minutes)
+    seconds.text = str(driver.lap_time.seconds)
