194fe3084c22322218548c9fcd0f201d00e10a58
Maks <gmb333x2@gmail.com>
2023-11-05 12:21:07 +0200

    Annotations added

--- app/cli/__main__.py
+++ app/cli/__main__.py
@@ -18,7 +18,7 @@ def cli():
 @click.option('--files', default=FOLDER_DATA, help='Path to the dir with logs')
 @click.option('--driver', help='Shows statistic about driver ')
 @click.option('--desc', is_flag=True, default=False, help='Descending order')
-def report(files, driver, desc):
+def report(files: str, driver: str, desc: bool) -> None:
     if not os.path.exists(files):
         raise ValueError(f"Path {files} from --file argument don't exist")
     build_from_parser(files, driver, desc)
@@ -28,7 +28,7 @@ def report(files, driver, desc):
 @click.option('--db_name', default=DB_NAME, help='Name of the database')
 @click.option('--drop', is_flag=True, help='Drop database')
 @click.option('--load', is_flag=True, help='Insert data to the database')
-def db(db_name, drop, load):
+def db(db_name: str, drop: bool, load: bool) -> None:
     if drop:
         return drop_database(DB_PATH, db_name)
 
